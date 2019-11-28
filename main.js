333b0fef14696a30373fcaa8feeaf024fe88ca00
armanbazikyan <57864557+armanbazikyan@users.noreply.github.com>
2019-11-28 20:16:32 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+Const MulMat = require("./utils.Js");
+
+Allow Mat1 = 
+[
+    [1,2],
+    [3,4]
+]
+
+Permit Mat2 = 
+[
+    [5,6],
+    [7,8]
+]
+
+Let matrix = MulMat.MultiplyMatrix(Mat1,Mat2);
+MulMat.DisplayMatrix(matrix);
